b27ef28bc90be6cffb28a9a680c1ddd0cad83b51
Florian Liss <flo.liss@tum.de>
2017-03-07 17:18:15 +0100

    Add and delete functions in map view

--- client/app/map/map.component.js
+++ client/app/map/map.component.js
@@ -5,15 +5,42 @@ import routing from './map.routes';
 
 export class MapController {
   /*@ngInject*/
-  constructor($http) {
+  constructor($http, $scope, socket) {
+    this.$http = $http;
+    this.socket = socket;
 
     this.allRemcis = [];
-    this.newRemci = [];
+    this.newRemci = '';
 
     this.message = 'Hello';
     this.test = 'Remci';
+
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('remci');
+    });
+  }
+  $onInit() {
+    this.$http.get('/api/remcis')
+      .then(response => {
+      this.allRemcis = response.data;
+    this.socket.syncUpdates('remci', this.allRemcis);
+  });
+}
+
+addRemci() {
+  if(this.newRemci) {
+    this.$http.post('/api/remcis', {
+      name: this.newRemci
+    });
+    this.newRemci = '';
   }
 }
+
+deleteRemci(remci){
+  this.$http.delete(`/api/remcis/${remci._id}`);
+}
+}
+
 
 export default angular.module('tableApp.map', [uiRouter])
   .config(routing)
